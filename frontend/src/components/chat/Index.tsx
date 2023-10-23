34648a1dbdf2ba9586c008d5aa3db7da3ffffffb
chinnawich <hotlnw159@gmail.com>
2023-10-23 16:31:24 +0700

    update `ui`

--- frontend/src/components/chat/Index.tsx
+++ frontend/src/components/chat/Index.tsx
@@ -12,9 +12,14 @@ export const Index = () => {
     <main className=" flex-row justify-between mt-2 h-5/6 md:h-5/6 md:flex  p-0 md:p-4  ">
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className={`flex justify-center items-center md:hidden ${isOpen ? "bg-slate-300 hover:bg-slate-800 transition-colors" : "bg-slate-800"} text-white rounded-full w-10 h-10 absolute z-[100] left-0 top-1/2 shadow-sm `}
+        className={`flex justify-center items-center md:hidden bg-slate-300 hover:bg-slate-800 transition-color 
+        text-white rounded-full w-10 h-10 absolute z-[100] left-0 top-1/2 shadow-sm `}
       >
-        {!isOpen ? <Icons.arrowRight className=" h-5 w-5"/> : <Icons.arrowLeft className=" h-5 w-5"/> }
+        {!isOpen ? (
+          <Icons.arrowRight className=" h-5 w-5" />
+        ) : (
+          <Icons.arrowLeft className=" h-5 w-5" />
+        )}
       </button>
       <ChatList isOpen={isOpen} />
       <Outlet />
